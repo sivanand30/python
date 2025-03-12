b76ecf077c82c729fb0a062a4982422710767088
sivanand30 <sivanammalsiva28@gmail.com>
2025-03-12 19:19:32 +0530

    list program

---
+++ p.list.py
@@ -0,0 +1,78 @@
+
+# listname=[]
+# print("empty list",listname)
+
+# listname=list()
+# print("empty list",listname)
+
+# list1=[1,2,3,4]
+# list2=[1,'a',"hello",12.4]
+# print("list1",list1)
+# print("list2",list2)
+
+# list1=list((10,20,30,40))
+# print(list1)
+
+# list1=[10,20,30,40,50]
+# print(list1[0])
+
+# list1=[10,20,30,40,50]
+# print(list1[-1])
+
+# list1=[10,20,30,40,50]
+# print(list1)
+
+# list1=[10,20,30,40,50]
+# for value in list1:
+#     print(value)
+
+# list1=[10,20,30,40,50]
+# for value in list1:
+#     print(value,end=" ")
+
+# str="sivanammal"
+# print(str)
+# strlist=list(str)
+# print(strlist)
+
+# set1=(22,33,44,55,66)
+# print(list(set1))
+
+# mydict={"name":"ram","age":"25","degree":"BCA"}
+# print(list(mydict))
+
+# a=[30,40,50,60]
+# a.insert(5,70)
+# print(a)
+
+# mylist=[100,200,300,400]
+# mylist.append(500)
+# print(mylist)
+
+# list1=[1,2,3]
+# list2=[4,5,6]
+# print(list1)
+# list1.append(list2)
+# print(list1)
+
+# x=['a','b','c']
+# y=['d','e','f']
+# x.extend(y)
+# print(x)
+
+# a=[11,22,33,44,55,66,77]
+# a.pop(5)
+# print(a)
+
+# a=[1,2,3,4,5,6,7,8,9]
+# a.remove(7)
+# print(a)
+
+# list1=['a','b','c']
+# list1.reverse()
+# print(list1)
+
+# x=[100,200,300,400,500,600]
+# x.sort()
+# print(x)
+
