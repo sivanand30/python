83e29f7e44a991d59d3688f940abd0ae1aa38820
sivanand30 <sivanammalsiva28@gmail.com>
2025-03-13 13:22:59 +0530

    Dictionaries

---
+++ P.Dictionary.py
@@ -0,0 +1,41 @@
+
+student={"name":"ram","age":20,"degree":"BCA"}
+print(student)
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# print(student['name'])
+# print(student['age'])
+
+# student["date of birth"]="10/10/2003"
+# print(student)
+
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# student["degree"]="BSC"
+# print(student)
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# student.pop("age")
+# print(student)
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# del student["age"]
+# print(student)
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# print(len(student))
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# if 'age' in student:
+#     print("age key exists")
+
+# student={"name":"ram","age":20,"degree":"BCA"}
+# for key in student:
+#     print(key,":",student[key])
+
+# student={"1styear":{"name":"ram","age":20},"2ndyear":{"name":"raja","age":21}}
+# print(student)
+
+# student={"1styear":{"name":"ram","age":20},"2ndyear":{"name":"raja","age":21}}
+# print(student["1styear"]["name"])
+
